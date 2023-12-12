48500b6efd00506748eb1649c33765fc4a3197af
Mneff19 <mneff@ehub.com>
2023-12-11 17:29:27 -0700

    added authentication to form submit and profile page loading

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,26 @@
-import JWTServices from "./modules/JWTServices.mjs";
+// import JWTServices from "./modules/JWTServices.mjs";
+import FileServices from "./modules/FileServices.mjs";
+import ApplicationServices from "./modules/ApplicationServices.mjs";
 
-const jwtServices = new JWTServices();
+const appServices = new ApplicationServices();
+const fileService = new FileServices();
+const creds = await fileService.getFileJSON('creds');
+// const jwtServices = new JWTServices();
 // jwtServices
+
+document.forms['loginForm'].addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.forms['loginForm'].username.value;
+    const password = document.forms['loginForm'].password.value;
+
+    if (Object.keys(creds).includes(username)) {
+        if (creds[username].password == password) {
+            appServices.setLocalItem('id', creds[username].id);
+            window.location = '/profile/index.html';
+        } else {
+            alert("Incorrect password, please try again.")
+        }
+    } else {
+        alert("Unknown username, please try another one.")
+    }
+})
--- assets/profile.js
+++ assets/profile.js
@@ -1,22 +1,38 @@
 import FileServices from "./modules/FileServices.mjs";
 import APIServices from "./modules/APIServices.mjs";
+import ApplicationServices from "./modules/ApplicationServices.mjs";
 
 const fileService = new FileServices();
-const profileInfo = await fileService.getFileJSON("profiles");
+const weatherAPI = new APIServices('https://api.openweathermap.org/data/2.5/forecast?lat=40.53183096915482&lon=-112.30080357417363&appid=84fd1805bbdfc8774858391be52c31e4&units=imperial');
+const appServices = new ApplicationServices();
 
-// Populate the profile info on the page
-for (const [profileProp, propVal] of Object.entries(profileInfo["1"])) {
-    if (profileProp != "profilePicUrl") {
-        document.querySelector(`#${profileProp}`).textContent = propVal;
-    } else {
-        document.querySelector(`#profilePic`).setAttribute("src", propVal);
-    }
+const jwt = appServices.getLocalItem('jwt');
+const jwtValid = false;
+if (jwt && jwtValid) {
+    document.querySelector(".s-profile").classList.add('authenticated');
+    await populateProfileInfo();
+    document.querySelectorAll(".animated-background").forEach((animatedEl) => {
+        animatedEl.classList.remove('animated-background');
+    });
 }
 
-const weatherAPI = new APIServices('https://api.openweathermap.org/data/2.5/forecast?lat=40.53183096915482&lon=-112.30080357417363&appid=84fd1805bbdfc8774858391be52c31e4&units=imperial');
-const weatherList = await weatherAPI.apiFetch();
+async function populateProfileInfo () {
+    const userID = appServices.getLocalItem('id');
+    const profileInfo = await fileService.getFileJSON("profiles");
+    const weatherList = await weatherAPI.apiFetch();
 
-// Display the results for the weather
-const desc = weatherList[0].weather[0].description;
-document.querySelector("#degrees").innerHTML = `${weatherList[0].main.temp}&deg;F`;
-document.querySelector("#weatherType").textContent = desc;
+    // Populate the profile info on the page
+    for (const [profileProp, propVal] of Object.entries(profileInfo[userID])) {
+        if (profileProp != "profilePicUrl") {
+            document.querySelector(`#${profileProp}`).textContent = propVal;
+        } else {
+            document.querySelector(`#profilePic`).setAttribute("src", propVal);
+            document.querySelector(`#profilePic`).setAttribute("alt", "Profile picture of " + profileInfo[userID].firstname);
+        }
+    }
+
+    // Display the results for the weather
+    const desc = weatherList[0].weather[0].description;
+    document.querySelector("#degrees").innerHTML = `${weatherList[0].main.temp}&deg;F`;
+    document.querySelector("#weatherType").textContent = desc;
+}
