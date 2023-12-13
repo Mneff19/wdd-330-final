001f22301bc9b79cd027e3daacb9586ca6040cad
Matthew Neff <mpneff19@gmail.com>
2023-12-12 23:32:45 -0700

    pointed file services to github so pages would work

--- src/modules/FileServices.mjs
+++ src/modules/FileServices.mjs
@@ -1,7 +1,7 @@
 export default class FileServices {
     async getFileJSON(filename) {
         try {
-            const response = await fetch(`../public/${filename}.json`);
+            const response = await fetch(`https://mneff19.github.io/wdd-330-final/public/${filename}.json`);
             return response.json();
         } catch (e) {
             // File ${filename}.json could not be found or did not contain valid JSON;
